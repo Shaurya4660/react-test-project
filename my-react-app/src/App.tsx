7950872b56cfc7384b0937fc7cf219f6fc03ee9e
Shaurya Vishnoi <165455780+Shaurya4660@users.noreply.github.com>
2026-02-06 13:59:50 +0530

    first change

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Hello shaurya how are you???</h1>
+      <h1>Vite + React Hello shaurya how are you??</h1>
       <h2>my first project push on bitbucket thanku</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
